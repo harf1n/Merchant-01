411042a8ce27ef3c2976fd2c46231e23add1d056
harf1n <bi4kax@gmail.com>
2022-04-13 18:21:59 +0300

    Add info method and describe calcBonus method

--- src/app/MerchantA.java
+++ src/app/MerchantA.java
@@ -8,4 +8,24 @@ public class MerchantA extends Merchant implements Bonus {
         super(name, phone);
         this.email = email;
     }
+    public String infoMerchant() {
+        String name = getName();
+        String phone = getPhone();
+        return "Продавец: " + name + ", " + phone + ", " + email + "\n";
+    }
+
+
+    // Метод этого класса переопределяющий метод интерфейса.
+    @Override
+    public double calcBonus(double sales) {
+        double bonus;
+        if (sales <= 10000) {
+            bonus = sales * 5 / 100;
+        } else if (sales > 10000 && sales <= 30000) {
+            bonus = sales * 10 / 100;
+        } else {
+            bonus = sales * 15 / 100;
+        }
+        return bonus;
+    }
 }
