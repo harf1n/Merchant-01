8d38ea25256a6a9e700420e66559b26a80fb7b08
harf1n <bi4kax@gmail.com>
2022-04-13 18:26:20 +0300

    Add Product obj model

--- src/app/Product.java
+++ src/app/Product.java
@@ -1,4 +1,13 @@
 package app;
 
 public class Product {
+    private final String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
