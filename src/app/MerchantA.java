8df9537c037e57058c8d34c47e5bb80975313ac2
harf1n <bi4kax@gmail.com>
2022-04-13 18:17:39 +0300

    Add extends to MerchantA with creating constructor and variable "email"

--- src/app/MerchantA.java
+++ src/app/MerchantA.java
@@ -1,4 +1,11 @@
 package app;
 
-public class MerchantA {
+public class MerchantA extends Merchant implements Bonus {
+    String email;
+
+    // Конструктор этого класса
+    public MerchantA(String name, String phone, String email) {
+        super(name, phone);
+        this.email = email;
+    }
 }
