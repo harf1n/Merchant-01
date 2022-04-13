441f67544bf3cafcd8bd9194bf7f28f31638cd50
harf1n <bi4kax@gmail.com>
2022-04-13 17:41:03 +0300

    Initialize variables

--- src/app/App.java
+++ src/app/App.java
@@ -1,4 +1,15 @@
 package app;
 
 public class App {
+    static String merchantName;
+    static String email;
+    static String phone;
+    static String productName;
+    static int quantity;
+    static double price;
+    static String roundBonus;
+    static ProductA product;
+    static MerchantA merchantA;
+    static String infoMerchant;
+    static String infoProduct;
 }
