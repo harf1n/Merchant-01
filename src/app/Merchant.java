40d58c2530c06dee9c7a42be0eade222821f1bce
harf1n <bi4kax@gmail.com>
2022-04-13 18:12:33 +0300

    Initialize variables in Merchant

--- src/app/Merchant.java
+++ src/app/Merchant.java
@@ -1,4 +1,11 @@
 package app;
 
 public class Merchant {
+    private final String name;
+    private final String phone;
+
+    public Merchant(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
 }
