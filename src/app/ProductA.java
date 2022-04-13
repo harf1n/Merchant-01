9d7665c4807a5f54838e8c3b5ab8702bfabe72b3
harf1n <bi4kax@gmail.com>
2022-04-13 18:28:50 +0300

    Add extends to ProductA and initialize variables

--- src/app/ProductA.java
+++ src/app/ProductA.java
@@ -1,4 +1,14 @@
 package app;
 
-public class ProductA {
+public class ProductA extends Product implements Sales {
+    int qnty;
+    double price;
+
+    // Конструктор этого класса
+    public ProductA(String name, int qnty, double price) {
+        super(name);
+        this.qnty = qnty;
+        this.price = price;
+    }
+
 }
